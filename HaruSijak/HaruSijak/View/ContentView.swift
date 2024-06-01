be1b7b08490942cf52f0f67faeeaf1267817138a
shin-na-ra <nas221@naver.com>
2024-06-01 23:18:00 +0900

    2024.06.01 snr : 첫 화면 생성!!

--- HaruSijak/HaruSijak/View/ContentView.swift
+++ HaruSijak/HaruSijak/View/ContentView.swift
@@ -7,17 +7,66 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    @State private var isPressed = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Spacer()
+                    
+                NavigationLink(destination: PredictView()) {
+                    Text("혼잡도 예측보기")
+                        .frame(width: 320, height: 200)
+                        .padding()
+                        .font(.system(.largeTitle, design: .rounded))
+                        .fontWeight(.black)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                        .scaleEffect(1.0)
+                }
+                
+                HStack(content: {
+                    NavigationLink(destination: NewsView()) {
+                        Text("뉴스\n요약 보기")
+                            .frame(width: 140, height: 150)
+                            .padding()
+                            .font(.system(.largeTitle, design: .rounded))
+                            .fontWeight(.black)
+                            .background(Color.orange)
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                            .scaleEffect(1.0)
+                        
+                    }
+                    
+                    NavigationLink(destination: TodoListView()) {
+                        Text("TodoList")
+                            .frame(width: 140, height: 150)
+                            .padding()
+                            .font(.system(.largeTitle, design: .rounded))
+                            .fontWeight(.black)
+                            .background(Color.pink)
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                            .scaleEffect(1.0)
+                        
+                    }
+                    
+                })
+                
+                
+                
+                Spacer()
+            }
+            .navigationTitle("하루시작")
         }
-        .padding()
     }
 }
+
 
 #Preview {
     ContentView()
