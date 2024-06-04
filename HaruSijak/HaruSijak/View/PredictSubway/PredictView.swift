285aecb705443daa64ba4b0290b199d858c72e23
shin-na-ra <nas221@naver.com>
2024-06-04 14:19:29 +0900

    2024.06.04 snr : 지하철혼잡도예측 페이지 생성

--- HaruSijak/HaruSijak/View/PredictSubway/PredictView.swift
+++ HaruSijak/HaruSijak/View/PredictSubway/PredictView.swift
@@ -3,14 +3,61 @@
 //  HaruSijak
 //
 //  Created by 신나라 on 6/1/24.
-//
+//  Description : 지하철 혼잡도 예측 페이지
 
 import SwiftUI
+import Zoomable
 
 struct PredictView: View {
+    
+    @State var stationName: String = ""
+    @FocusState var isTextFieldFocused: Bool
+    
     var body: some View {
-        Text("혼잡도 예측페이지")
-    }
+        
+        
+        VStack {
+            
+            Spacer()
+            
+            // 검색창 TextField
+            HStack(content: {
+                TextField("지하철 7호선 역명, 초성 검색", text : $stationName)
+                    .padding()
+                    .frame(width: 280)
+                    .textFieldStyle(.roundedBorder)
+                    .multilineTextAlignment(.leading)
+                    .keyboardType(.default)
+                    .focused($isTextFieldFocused)
+                
+                // 검색버튼
+                Button(action: {
+                    //action
+                }, label: {
+                    Image(systemName: "magnifyingglass")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                })//Button
+            })//Hstack
+            
+            //스크롤뷰 [노선도 사진]
+            ScrollView(.vertical, content: {
+                ScrollView(.horizontal ,content: {
+                   Image("line73")
+                        .resizable()
+                        .frame(width: 1500, height: 1000)
+                        .zoomable() // double click시 화면 확대
+                })
+                .frame(maxWidth: .infinity)
+            })
+            
+            
+        } //Vstack
+        
+        
+        
+    } //body
+        
 }
 
 #Preview {
