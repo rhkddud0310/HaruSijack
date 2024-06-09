5f793248056ddfd7eac7aa72c58668ee81ea33f1
shin-na-ra <nas221@naver.com>
2024-06-09 16:12:23 +0900

    2024.06.09 snr : todoLists 상세보기 및 수정기능 완료

--- HaruSijak/HaruSijak/View/TodoList/TodoListView.swift
+++ HaruSijak/HaruSijak/View/TodoList/TodoListView.swift
@@ -21,48 +21,55 @@ struct TodoListView: View {
     }()
     
     var body: some View {
-        NavigationView(content: {
-            List(content: {
-                ForEach(todoLists, id:\.id, content: { todoList in
-                    NavigationLink(destination: DetailTodoListView(todoList: todoList), label: {
-                        VStack(content: {
-                            Text(todoList.todo)
-                                .font(.system(size: 14))
+
+        VStack {
+            
+//            Text("TodoList")
+//                .font(.system(.largeTitle, design: .rounded))
+//                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            
+            NavigationView(content: {
+                List(content: {
+                    ForEach(todoLists, id:\.id, content: { todoList in
+                        NavigationLink(destination: DetailTodoListView(todoList: todoList), label: {
+                            VStack(alignment:.leading ,content: {
+                                Text(todoList.todo)
+                                    .font(.system(size: 14))
+                                    .bold()
+                                
+                                HStack(content: {
+                                    Text(dateFormatter.string(from: todoList.startdate))
+                                    Text("-")
+                                    Text(dateFormatter.string(from: todoList.enddate))
+                                })
+                                .foregroundStyle(todoList.status == 1 ? Color.black.opacity(0.3) : Color.black)
+                                .padding(.top, 5)
+                                .font(.system(size: 12))
+                            })//Vstack
+                        })// NavigationLink
+                    })//ForEach
+                })// List
+                .onAppear(perform: {
+                    todoLists.removeAll() //지우고
+                    let todoListDB = TodoListDB() // DB instance 생성
+                    todoLists = todoListDB.queryDB() //VM에 있는 쿼리 실행
+                })
+                .navigationTitle("TodoList")
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationBarBackButtonHidden(true)
+                //추가 아이콘
+                .toolbar(content: {
+                    ToolbarItem(placement: .topBarTrailing, content: {
+                        // 할일 추가로 이동
+                        NavigationLink(destination: AddTodoListView()) {
+                            Image(systemName: "plus")
+                                .padding(.trailing, 10)
                             
-                            HStack(content: {
-                                Text(dateFormatter.string(from: todoList.startdate))
-                                Text("-")
-                                Text(dateFormatter.string(from: todoList.enddate))
-                            })
-                            .foregroundStyle(todoList.status == 1 ? Color.black.opacity(0.3) : Color.black)
-                            .padding(.top, 5)
-                            .font(.system(size: 15))
-                            
-                        })//Vstack
-                    })// NavigationLink
-                })//ForEach
-            })// List
-            .onAppear(perform: {
-                todoLists.removeAll() //지우고
-                let todoListDB = TodoListDB() // DB instance 생성
-                todoLists = todoListDB.queryDB() //VM에 있는 쿼리 실행
+                        } // NavigationLink
+                    }) // ToolbarItem
+                })
             })
-            .navigationTitle("TodoList")
-            .navigationBarTitleDisplayMode(.large)
-                
-            //추가 아이콘
-            .toolbar(content: {
-                ToolbarItem(placement: .topBarTrailing, content: {
-                    // 할일 추가로 이동
-                    NavigationLink(destination: AddTodoListView()) {
-                        Image(systemName: "plus")
-                            .padding(.trailing, 10)
-                        
-                    } // NavigationLink
-                }) // ToolbarItem
-            })
-//            .navigationBarHidden(true)
-        })// NavigationView
+        }// NavigationView
     }
 }
 
