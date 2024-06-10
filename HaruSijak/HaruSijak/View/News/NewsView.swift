3c5584d2a8a4a98c0eece1285da35aadf91ad87b
pulpilisory <pulpilisory@gmailcom>
2024-06-10 14:11:50 +0900

    pdg : Flask 와 swift rest api 연습및 ui review 용 test 페이지 만듬

--- HaruSijak/HaruSijak/View/News/NewsView.swift
+++ HaruSijak/HaruSijak/View/News/NewsView.swift
@@ -1,9 +1,13 @@
-//
-//  NewsView.swift
-//  HaruSijak
-//
 //  Created by 신나라 on 6/1/24.
-//
+// MARK: -- Description
+/*
+    Description : HaruSijack App 개발 News Crolling page
+    Date : 2024.6.1
+    Author :
+    Dtail :
+    Updates :
+        * 2024.06.10 by
+ */
 
 import SwiftUI
 
