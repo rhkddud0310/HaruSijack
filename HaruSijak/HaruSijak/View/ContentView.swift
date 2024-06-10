51ba2798b024fead6facf1d17bcf1e14d9afff12
pulpilisory <pulpilisory@gmailcom>
2024-06-10 13:43:39 +0900

    * 2024.06.10 by pdg : 기존 페이지에서 tabbar view 추가 
     * 2024.06.10 by pdg : 기존 페이지에서 tabbar view 추가 
                - 프로그램 코드 기본 주석, 포맷 작성
                - tabbar 생성

--- HaruSijak/HaruSijak/View/ContentView.swift
+++ HaruSijak/HaruSijak/View/ContentView.swift
@@ -5,82 +5,63 @@
     Date : 2024.6.1
     Author : Shin  + pdg
     Dtail :
-    Update :
+    Updates :
         * 2024.06.10 by pdg : 기존 페이지에서 tabbar view 추가 
+            - 프로그램 코드 기본 주석, 포맷 작성
+            - tabbar 생성 
  */
 
 //
 
 import SwiftUI
-
-
 struct ContentView: View {
     // MARK: -- properties
     @State private var isPressed = false
-    
+    @State var selection = 0
+    // MARK: -- body
     var body: some View {
         NavigationView {
             VStack {
                 Spacer()
-                
                 Text("하루시작")
                     .font(.system(.largeTitle, design: .rounded))
                     .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                
                 Spacer()
-                
-                NavigationLink(destination: PredictView()) {
-                    ZStack(content: {
-                        
-                        // 배경 이미지
-                        Image(systemName: "tram.fill")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 200, height: 300)
-                            .opacity(0.3)
-                        
-                        // 버튼 텍스트
-                        Text("지하철 혼잡도\n예측보기")
-                            .multilineTextAlignment(.center)
-                            .frame(width: 320, height: 200)
-                            .padding()
-                            .font(.system(.largeTitle, design: .rounded))
-                            .fontWeight(.black)
-                            .background(Color.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(8)
-                    })// Z
-                }//NL
-                
-                HStack(content: {
-                    NavigationLink(destination: NewsView()) {
-                        Text("뉴스\n요약 보기")
-                            .frame(width: 140, height: 150)
-                            .padding()
-                            .font(.system(.largeTitle, design: .rounded))
-                            .fontWeight(.black)
-                            .background(Color.orange)
-                            .foregroundColor(.white)
-                            .cornerRadius(8)
-                            .scaleEffect(1.0)
-                    }
-                    NavigationLink(destination: TodoListView()) {
-                        Text("TodoList")
-                            .frame(width: 140, height: 150)
-                            .padding()
-                            .font(.system(.largeTitle, design: .rounded))
-                            .fontWeight(.black)
-                            .background(Color.pink)
-                            .foregroundColor(.white)
-                            .cornerRadius(8)
-                            .scaleEffect(1.0)
-                    }//NL
-                })//H
+                // TabBarview
+                TabView(selection: $selection,content:  {
+                        Group(content: {
+                            PredictView()
+                                .tabItem {
+                                    Image(systemName: "tram")
+                                    Text("혼잡도")
+                                }
+                                .tag(0)
+                            NewsView()
+                                .tabItem {
+                                    Image(systemName: "newspaper")
+                                    Text("뉴스")
+                                }
+                                .tag(1)
+                            TodoListView()
+                                .tabItem {
+                                    Image(systemName: "calendar")
+                                    Text("할일")
+                                }
+                                .tag(2)
+                        })//G
+                        .toolbarBackground(.visible, for: .tabBar)
+
+                })//TV
                 Spacer()
-            }// V
+            }// VS
         } //NV
-    }//View
-}
+    }// Body
+    //MARK: FUNCTIONS
+    
+}// CV
+
+//MARK: Structures
+
 
 
 #Preview {
