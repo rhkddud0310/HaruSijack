44927b8bc60fff0174fe46afcb2ceac854addb6a
zlzlwn <zlzlwn1018@gmail.com>
2024-06-15 00:58:59 +0900

    코드 refactoring중 commit
    코드 refactoring중 commit(컴포넌트별로 나누기 전 commit)

--- HaruSijak/HaruSijak/View/Splash/SplashView.swift
+++ HaruSijak/HaruSijak/View/Splash/SplashView.swift
@@ -1,33 +1,44 @@
 
 // MARK: -- Description
 /*
-    Description : HaruSijack App splash 화면
-    Date : 2024.6.14
-    Author : j.park
-    Dtail :
-    Updates :
-        * 2024.06.14 by j.park : 1.splash화면 생성
-                                 2. Lotti 적용
-    
+ Description : HaruSijack App splash 화면
+ Date : 2024.6.14
+ Author : j.park
+ Dtail :
+ Updates :
+ * 2024.06.14 by j.park : 1.splash화면 생성
+                          2. Lotti 적용
+ 
  */
 
 //
 
 import SwiftUI
+import Lottie
 
 struct SplashView: View {
     @State private var isActive = false
-
+    
     var body: some View {
         VStack {
+            
             Spacer()
-            Text("HaruSijack")
-                .font(.system(size: 40, weight: .bold, design: .rounded))
-                .padding()
+            ZStack {
+                LottieView(jsonName: "train2")
+                    .frame(width: 250, height: 300)
+                Spacer()
+                Text("HaruSijack")
+                    .font(.system(size: 40, weight: .bold, design: .rounded))
+                    .foregroundColor(.black)
+                    .offset(y: 100)
+            }
+            
             Spacer()
+            Spacer()
+            
         }
         .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
                 withAnimation {
                     self.isActive = true
                 }
