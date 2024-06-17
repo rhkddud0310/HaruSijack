2116a78b15da0f6133dc0e5ab4ba28f4f93f7a5b
shin-na-ra <nas221@naver.com>
2024-06-17 15:06:44 +0900

    2024.06.17 snr : 데이터 추가, 수정, 삭제 시 달력에 바로 적용되도록 onDismiss 처리함

--- HaruSijak/HaruSijak/View/Schedule/CustomDatePicker.swift
+++ HaruSijak/HaruSijak/View/Schedule/CustomDatePicker.swift
@@ -16,6 +16,8 @@
                          - 날짜 클릭 시 Capsule() -> Circle() 모양으로 변경
                             => 비교 후에 삭제 예정
                          - 달력 사이즈 조정
+ 
+    - 2024. 06. 17 snr - 추가 후에 달력에 바로 반영되기 위해서 onDismiss 처리
                          
  */
 
@@ -33,6 +35,14 @@ struct CustomDatePicker: View {
     @State var isPresented = false //상세보기 sheet 조회 변수
     @Binding var dateValue : Date
     @State var selectedTask: Task? = nil // ForEach로 생성된 리스트의 task 값을 담을 변수
+    
+    @State var isAlert = false            // actionSheet 실행
+    @State var isSubAlert = false            // subAlert 실행
+    @State var isResultTrue = false
+    @State var isResultFalse = false
+    @State var date: Date = Date()              // 선택된 날짜 변수
+    @State var time: Date = Date()              // 선택된 시간 변수
+    @State var task: String = ""
     
     
     var body: some View {
@@ -156,11 +166,101 @@ struct CustomDatePicker: View {
             print("onAppear 실행")
             fetchTasksForSelectedDate()
         }
-        .sheet(item: $selectedTask, content: { task in
+        .sheet(item: $selectedTask, onDismiss:  {
+            fetchTasksForSelectedDate()
+        }, content: { task in
             CalendarDetailView(task: task, currentDate: dateValue)
                 .presentationDetents([.medium])
                 .presentationDragIndicator(.visible)
         })
+        .safeAreaInset(edge: .bottom, content: {
+            HStack(content: {
+                Button(action: {
+                    isAlert = true
+                }, label: {
+                    Image(systemName: "plus")
+                        .foregroundStyle(.white)
+                        .fontWeight(.bold)
+                        .frame(width: 100)
+                        .padding(.vertical)
+                        .background(Color("myColor"), in: Circle())
+                })
+                .sheet(isPresented: $isAlert, onDismiss: {
+                    fetchTasksForSelectedDate()
+                }, content: { VStack(content: {
+                        
+                        //일정 textfield
+                        TextField("일정 제목", text: $task)
+                            .font(.title3.bold())
+                            .frame(width: 200)
+                            .padding()
+                            .cornerRadius(8)
+                            .focused($isTextFieldFocused)
+                        
+                        //요일 설정 picker
+                        DatePicker(
+                            "요일 설정 : ",
+                            selection: $date,
+                            displayedComponents: [.date]
+                        )
+                        .frame(width: 200)
+                        .environment(\.locale, Locale(identifier: "ko_KR")) // 한국어로 설정
+                        .tint(Color("color1"))
+                        
+                        //시간 설정 picker
+                        DatePicker(
+                            "시간 설정 : ",
+                            selection: $time,
+                            displayedComponents: [.hourAndMinute]
+                        )
+                        .frame(maxWidth: 200)
+                        .environment(\.locale, Locale(identifier: "ko_KR")) // 한국어로 설정
+                        .tint(Color("color1"))
+                        
+                        //추가 버튼
+                        Button("추가하기", action: {
+                            if task != "" {
+                                let newTask = Task(id: UUID().uuidString, title: task, time: time)
+                                dbModel.insertDB(task: newTask, taskDate: date)
+                                isAlert = false //alert창 닫기
+                            } else {
+                                isSubAlert = true
+                            }
+                            
+                            //추가 후 초기화처리
+                            task = ""
+                            date = Date()
+                            time = Date()
+                        }) // Button
+                        .tint(.white)
+                        .buttonStyle(.bordered)
+                        .buttonBorderShape(.capsule)
+                        .background(Color("color1"))
+                        .cornerRadius(30)
+                        .controlSize(.large)
+                        .frame(width: 200, height: 50) // 버튼의 크기 조정
+                        .padding(.top, 40)
+                        
+                        // 일정 == empty일 때, alert처리
+                        .alert(isPresented: $isSubAlert) {
+                            Alert(
+                                title: Text("경고"),
+                                message: Text("일정을 작성해주세요."),
+                                dismissButton: .default(Text("확인"), action: {
+                                    isSubAlert = false
+                                })
+                            )
+                        }// alert
+                    })
+                    .presentationDetents([.medium])
+                    .presentationDragIndicator(.visible)
+                }) //sheet
+            })
+            .padding(.horizontal)
+            .background(.clear)
+            .padding(.bottom, 10)
+            
+        })//safeArea
         
     }// body
     
