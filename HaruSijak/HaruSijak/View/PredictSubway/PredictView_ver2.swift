a234fb7c4c8027d1b5b6214e2ece5ae59bb39832
pulpilisory <pulpilisory@gmailcom>
2024-06-23 16:20:32 +0900

    pdg  predict 2 test and magnification

--- HaruSijak/HaruSijak/View/PredictSubway/PredictView_ver2.swift
+++ HaruSijak/HaruSijak/View/PredictSubway/PredictView_ver2.swift
@@ -6,15 +6,46 @@
 //
 
 import SwiftUI
+import Zoomable
+import Charts
 
 struct PredictView_ver2: View {
-    let sublist = SubwayList()
+    let line5 = SubwayList().stations_line5
+    let image = UIImage(named: "subwayMap")
+    
+    let imgWidth = UIImage(named: "subwayMap")!.size.width
+    let imgHight = UIImage(named: "subwayMap")!.size.height
     
     
     var body: some View {
-        Text("\(sublist.stations_line5[0].0)")
-        Text("test")
-        Image("subwayMap")
+//        Text("\(sublist.stations_line5[0].0)")
+        Text("\(image!.size.width),\(image!.size.height)")
+        ScrollView([.horizontal, .vertical],
+                   showsIndicators: false,
+                   content: {
+            Image("subwayMap")
+                .resizable()
+                .frame(width: imgWidth, height: imgHight)
+                .overlay(
+                    GeometryReader {geometry in
+                        ForEach(line5, id: \.0) { station in
+                            Button(action: {
+                                print("button clicked")
+                            }, label: {
+                                Text("")
+                                    .background(Color.red)
+                                
+                            }
+                                
+                            )}
+                        
+                        
+                    }
+                )
+            
+        })
+        
+        
     }
 }
 
