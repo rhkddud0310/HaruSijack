b08075d46a4509dc2edce0b2ec3e81e9aa24e20a
pulpilisory <pulpilisory@gmailcom>
2024-06-26 17:02:59 +0900

    pdg news update

--- Harusijack_v.3.1.2/HaruSijak/View/News/NewsView.swift
+++ Harusijack_v.3.1.2/HaruSijak/View/News/NewsView.swift
@@ -25,74 +25,52 @@ struct NewsView: View {
   // MARK: * Property *
   @State var newsList: [NewsModel] = []
   
-  let columns = [
-    GridItem(),
-    GridItem()
-//    GridItem(.flexible()),
-//    GridItem(.flexible())
-  ]
-  
   var body: some View {
       
-      ScrollView {
-        LazyVGrid(columns: columns, spacing: 10) {
-          ForEach(newsList, id: \.self) { news in
-            NewsCell(newsTitle: news.Title, newsPress: news.Press)
-          } // end of closure ForEach {news in}
-          
-        } // end of LazyVGrid
-        .padding()
-        
-      } // end of ScrollView
-      .navigationTitle("A")
-      .navigationBarTitleDisplayMode(/*@START_MENU_TOKEN@*/.automatic/*@END_MENU_TOKEN@*/)
-    .onAppear {
-      
-      let newsVM = NewsVM()
-      
-      newsVM.loadData(url: URL(string: "http://127.0.0.1:5000/news")!) { result in
-        switch result {
-        case .success(let news):
-          DispatchQueue.main.async {
-            newsList = news
-          }
-        case .failure(let error):
+      NavigationView(content: {
+          ScrollView(content: {
+              VStack(alignment:.leading,content: {
+                  ForEach(newsList, id: \.self) { news in
+                      NavigationLink(destination: NewsArticleView(newslink: news.Link), label: {
+                          VStack(alignment:.leading,content:{
+                              Text(news.Press)
+                                .font(.headline)
+                                .padding([.top, .leading, .trailing])
+                                .foregroundColor(.primary)
+                              
+                              Text(news.Title)
+                                .font(.subheadline)
+                                .padding([.leading, .bottom, .trailing])
+                                .frame(alignment:.leading)
+                                .foregroundStyle(Color.purple)
+                          })//VS
+                        .background(Color.white)
+                        .cornerRadius(8)
+                        .shadow(radius: 4)
+                        .padding([.top, .horizontal])
+                      })//NVLInk
+                      .buttonStyle(PlainButtonStyle())
+                  }//FE
+              })//VS
+          })//SV
+          .navigationTitle("오늘의 뉴스")
+          .navigationBarTitleDisplayMode(/*@START_MENU_TOKEN@*/.automatic/*@END_MENU_TOKEN@*/)
+      })//NV
+      .onAppear {
+          let newsVM = NewsVM()
+          newsVM.loadData(url: URL(string: "http://127.0.0.1:5000/news")!) { result in
+              switch result {
+              case .success(let news):DispatchQueue.main.async {
+                  newsList = news
+              }
+              case .failure(let error):
           print("Error loading news: \(error.localizedDescription)")
         } // end of switch result 구문
-        
       } // end of closure newsVM.loadData {result in}
-      
     } // end of .onAppear
-    
   } // end of var body: some View
-  
 } // end of struct NewsView: View
 
-
-struct newsCell : View {
-    
-    // MARK: * Property *
-    let newsTitle: String
-    let newsPress: String
-    
-    var body: some View {
-        VStack(alignment: .leading) {
-          
-          Text(newsPress)
-            .font(.headline)
-            .padding([.top, .leading, .trailing])
-          
-          Text(newsTitle)
-            .font(.subheadline)
-            .padding([.leading, .bottom, .trailing])
-          
-        } // end of VStack
-        .background(Color.white)
-        .cornerRadius(8)
-        .shadow(radius: 4)
-        .padding([.top, .horizontal])
-    }
-}
 
 #Preview {
   NewsView()
